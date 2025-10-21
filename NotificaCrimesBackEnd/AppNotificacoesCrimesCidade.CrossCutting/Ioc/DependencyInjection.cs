9f66380842271516131eba15edfb19eafaa849c8
Antonio Zago <antoniozagodev@gmail.com>
2025-10-20 22:54:49 -0300

    NC-1.4 - Criação do projeto de backend

---
+++ NotificaCrimesBackEnd/AppNotificacoesCrimesCidade.CrossCutting/Ioc/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using AppNotificacoesCrimesCidade.Infraestructure.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AppNotificacoesCrimesCidade.CrossCutting.Ioc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfigurationManager configuration)
+        {
+            var postgresConnection = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(postgresConnection));
+           
+            return services;
+        }
+    }
+}
